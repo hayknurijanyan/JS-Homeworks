84cfbfe8ca84a5927341f14e7bb0686512b769ac
unknown <hayknurijanyan@gmail.com>
2020-07-01 01:56:29 +0400

    homework2

--- Homework1.js
+++ Homework1.js
@@ -1,1 +1,17 @@
-console.log ('hello world')
+/* ex 2
+ Given an array of numbers. Print frequency of each unique number. (Frequency is the
+ count of particular element divided by the count of all elements)*/
+arr1 = [1,1,2,2,3]
+
+
+function frequency (arr,number){
+  let count =0;
+  for (let i =0; i<=arr.length; i++){
+    if (arr[i] === number){count+=1}
+    }
+  return `${number}: ${count/arr.length}`
+  }
+
+console.log(frequency(arr1,1))
+console.log(frequency(arr1,2))
+console.log(frequency(arr1,3))
