e37238c3f554a1145bce0868550ec4d905346e58
unknown <hayknurijanyan@gmail.com>
2020-07-01 13:17:27 +0400

    ex3 completed

--- Homework1.js
+++ Homework1.js
@@ -16,3 +16,22 @@ console.log(frequency(arr1,1))
 console.log(frequency(arr1,2))
 console.log(frequency(arr1,3))
 
+
+/* ex 3
+Given an array of strings and numbers.
+Print the number of integers and the number
+of strings in the array.
+*/
+
+let arr1 = [1,2,'hi',8,'11',3]
+
+function numberAndStringCount (arr){
+  let number =0;
+  let string =0;
+  for (let i=0;i<=arr.length; i++){
+      if (typeof arr[i] === 'number'){number+=1}
+      else if (typeof arr[i] === 'string'){string+=1}
+  } return `Numbers:${number} Strings:${string}`
+}
+
+console.log(numberAndStringCount(arr1))
